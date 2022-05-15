d16a26215f95327518893e0807dc96fd247bd723
MrMeky <76445665+MrMeky@users.noreply.github.com>
2022-05-15 22:46:42 +0200

    Added flying and chasing enemy
    Simple flying and chasing enemy types. Make sure that level scene has Navigation2D node in order to make chaser work.

---
+++ src/Enemy_Flying/Enemy_Flying.gd
@@ -0,0 +1,20 @@
+extends KinematicBody2D
+const SPEED = 50
+
+
+var velocity = Vector2()
+var direction = 1
+
+func _ready():
+	$Timer.start()
+
+func _physics_process(delta):
+	velocity.x = SPEED * direction
+	$AnimationPlayer.play("Fly")
+	velocity = move_and_slide(velocity)
+	
+	if $Timer.time_left == 0:
+		direction *= -1
+		$Sprite.flip_h = !$Sprite.flip_h 
+		$Timer.start()	
+
