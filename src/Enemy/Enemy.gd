c9f00bac213756d332c45033fc00b7e07e3cb3a5
MrMeky <76445665+MrMeky@users.noreply.github.com>
2022-05-15 17:37:47 +0200

    Added simple walking Enemy

---
+++ src/Enemy/Enemy.gd
@@ -0,0 +1,26 @@
+extends KinematicBody2D
+const GRAVITY = 10
+const SPEED = 50
+const FLOOR = Vector2(0,-1)
+
+var velocity = Vector2()
+var direction = 1
+
+func _ready():
+	pass
+
+func _physics_process(delta):
+	velocity.x = SPEED * direction
+	$AnimationPlayer.play("Walk Right")
+	velocity.y += GRAVITY
+	velocity = move_and_slide(velocity,FLOOR)
+	
+	if is_on_wall():
+		direction *= -1
+		$Sprite.flip_h = !$Sprite.flip_h 
+		$RayCast2D.position.x *= -1
+	
+	if $RayCast2D.is_colliding() == false:
+		direction *= -1
+		$Sprite.flip_h = !$Sprite.flip_h 
+		$RayCast2D.position.x *= -1
