7cc905838daa72837d6dd8fd4e578eda159b48a7
grzedzicki <49614558+grzedzicki@users.noreply.github.com>
2022-06-13 20:12:31 +0200

    New main menu look

--- src/GUI/Menu.gd
+++ src/GUI/Menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _ready():
-	$VBoxContainer/StartGameButton.grab_focus()
+	$StartGameButton.grab_focus()
 
 func _on_StartGameButton_pressed():
 	get_tree().change_scene("res://src/levels/level_1.tscn") #Mapka Macieja do wstawienia tutaj
